a4c0077f998fd4f50324734fce2487e0f8ab70f7
ranyong <ranyong1209@gmail.com>
2023-04-14 11:01:25 +0800

    ✨feat:(Added data import management):新增数据导入管理

--- utils/excel/excel_manage.py
+++ utils/excel/excel_manage.py
@@ -20,6 +20,7 @@ from application.settings import TEMP_DIR, TEMP_URL
 openpyxl常用用法及封装：https://blog.csdn.net/beichanbb/article/details/127887605
 """
 
+
 class ExcelManage:
     """
     Excel文件序列化
@@ -32,183 +33,171 @@ class ExcelManage:
         self.sheet = None
         self.wb = None
 
-    class ExcelManage:
+    def open_workbook(self, file: str, read_only: bool = False, data_only: bool = False) -> None:
         """
-        excel 文件序列化
+        初始化 excel 文件
+
+        :param file: 文件名称或者对象
+        :param read_only: 是否只读，优化读取速度
+        :param data_only: 是否加载文件对象
         """
+        # 加载excel文件，获取表单
+        self.wb = load_workbook(file, read_only=read_only, data_only=data_only)
 
-        # 列名，A-Z
-        EXCEL_COLUMNS = [chr(a) for a in range(ord('A'), ord('Z') + 1)]
+    def open_sheet(self, sheet_name: str = None, **kwargs) -> None:
+        """
+        初始化 excel 文件
 
-        def __init__(self):
-            self.sheet = None
-            self.wb = None
-
-        def open_workbook(self, file: str, read_only: bool = False, data_only: bool = False) -> None:
-            """
-            初始化 excel 文件
-
-            :param file: 文件名称或者对象
-            :param read_only: 是否只读，优化读取速度
-            :param data_only: 是否加载文件对象
-            """
-            # 加载excel文件，获取表单
-            self.wb = load_workbook(file, read_only=read_only, data_only=data_only)
-
-        def open_sheet(self, sheet_name: str = None, **kwargs) -> None:
-            """
-            初始化 excel 文件
-
-            :param sheet_name: 表单名称，为空则默认第一个
-            """
-            # 加载excel文件，获取表单
-            if not self.wb:
-                self.open_workbook(kwargs.get("file"), kwargs.get("read_only", False), kwargs.get("data_only", False))
-            if sheet_name:
-                self.sheet = self.wb[sheet_name]
-            else:
-                self.sheet = self.wb.active
-
-        def get_sheets(self) -> list:
-            """
-            读取所有工作区名称
-            :return: 一维数组
-            """
-            return self.wb.sheetnames
-
-        def create_excel(self, sheet_name: str = None) -> None:
-            """
-            创建 excel 文件
-
-            :param sheet_name: 表单名称，为空则默认第一个
-            """
-            # 加载excel文件，获取表单
-            self.wb = Workbook()
+        :param sheet_name: 表单名称，为空则默认第一个
+        """
+        # 加载excel文件，获取表单
+        if not self.wb:
+            self.open_workbook(kwargs.get("file"), kwargs.get("read_only", False), kwargs.get("data_only", False))
+        if sheet_name:
+            self.sheet = self.wb[sheet_name]
+        else:
             self.sheet = self.wb.active
-            if sheet_name:
-                self.sheet.title = sheet_name
 
-        def readlines(self, min_row: int = 1, min_col: int = 1, max_row: int = None, max_col: int = None) -> list:
-            """
-            读取指定表单所有数据
+    def get_sheets(self) -> list:
+        """
+        读取所有工作区名称
+        :return: 一维数组
+        """
+        return self.wb.sheetnames
 
-            :return: 二维数组
-            """
-            rows = self.sheet.iter_rows(min_row=min_row, min_col=min_col, max_row=max_row, max_col=max_col)
-            result = []
-            for row in rows:
-                _row = []
-                for cell in row:
-                    _row.append(cell.value)
-                if any(_row):
-                    result.append(_row)
-            return result
+    def create_excel(self, sheet_name: str = None) -> None:
+        """
+        创建 excel 文件
 
-        def get_header(self, row: int = 1, col: int = None, asterisk: bool = False) -> list:
-            """
-            读取指定表单的表头（第一行数据）
+        :param sheet_name: 表单名称，为空则默认第一个
+        """
+        # 加载excel文件，获取表单
+        self.wb = Workbook()
+        self.sheet = self.wb.active
+        if sheet_name:
+            self.sheet.title = sheet_name
 
-            :param row: 指定行
-            :param col: 最大列
-            :param asterisk: 是否去除 * 号
-            :return: 一维数组
-            """
-            rows = self.sheet.iter_rows(min_row=row, max_col=col)
-            result = []
-            for row in rows:
-                for cell in row:
-                    value = cell.value.replace("*", "").strip() if asterisk else cell.value.strip()
-                    result.append(value)
-                break
-            return result
+    def readlines(self, min_row: int = 1, min_col: int = 1, max_row: int = None, max_col: int = None) -> list:
+        """
+        读取指定表单所有数据
 
-        def write_list(self, rows: list, header: list = None) -> None:
-            """
-            写入 excel文件
+        :return: 二维数组
+        """
+        rows = self.sheet.iter_rows(min_row=min_row, min_col=min_col, max_row=max_row, max_col=max_col)
+        result = []
+        for row in rows:
+            _row = []
+            for cell in row:
+                _row.append(cell.value)
+            if any(_row):
+                result.append(_row)
+        return result
 
-            :param rows: 行数据集
-            :param header: 表头
-            """
-            if header:
-                self.sheet.append(header)
-            for index, data in enumerate(rows):
-                format_columns = {
-                    "date_columns": []
-                }
-                for i in range(0, len(data)):
-                    if isinstance(data[i], datetime.datetime):
-                        data[i] = data[i].strftime("%Y/%m/%d %H:%M:%S")
-                        format_columns["date_columns"].append(i + 1)
-                self.sheet.append(data)
-                self.__set_row_style(index + 2, len(data) - 1)
-                self.__set_row_format(index + 2, format_columns)
-            self.__auto_width()
+    def get_header(self, row: int = 1, col: int = None, asterisk: bool = False) -> list:
+        """
+        读取指定表单的表头（第一行数据）
 
-        def save_excel(self, filename: str = None):
-            """
-            保存 excel 文件到本地
-            默认保存到临时目录中
-            """
-            date = datetime.datetime.strftime(datetime.datetime.now(), "%Y%m%d")
-            file_dir = os.path.join(TEMP_DIR, date)
-            if not os.path.exists(file_dir):
-                os.mkdir(file_dir)
-            if not filename:
-                name = hashlib.md5(str(random.random()).encode()).hexdigest() + ".xlsx"
-                filename = os.path.join(file_dir, name)
-            else:
-                name = filename
-                filename = os.path.join(file_dir, filename)
-            self.wb.save(filename)
-            # 返回访问路由
-            return f"{TEMP_URL}/{date}/{name}"
+        :param row: 指定行
+        :param col: 最大列
+        :param asterisk: 是否去除 * 号
+        :return: 一维数组
+        """
+        rows = self.sheet.iter_rows(min_row=row, max_col=col)
+        result = []
+        for row in rows:
+            for cell in row:
+                value = cell.value.replace("*", "").strip() if asterisk else cell.value.strip()
+                result.append(value)
+            break
+        return result
 
-        def __set_row_style(self, row: int, max_column: int):
-            """
-            设置行样式
+    def write_list(self, rows: list, header: list = None) -> None:
+        """
+        写入 excel文件
 
-            :param row: 行
-            :param max_column: 最大列
-            """
-            for index in range(0, max_column):
-                # 设置单元格对齐方式
-                # Alignment(horizontal=水平对齐模式,vertical=垂直对齐模式,text_rotation=旋转角度,wrap_text=是否自动换行)
-                alignment = Alignment(horizontal='center', vertical='center', text_rotation=0, wrap_text=False)
-                self.sheet.cell(row=row, column=index + 1).alignment = alignment
+        :param rows: 行数据集
+        :param header: 表头
+        """
+        if header:
+            self.sheet.append(header)
+        for index, data in enumerate(rows):
+            format_columns = {
+                "date_columns": []
+            }
+            for i in range(0, len(data)):
+                if isinstance(data[i], datetime.datetime):
+                    data[i] = data[i].strftime("%Y/%m/%d %H:%M:%S")
+                    format_columns["date_columns"].append(i + 1)
+            self.sheet.append(data)
+            self.__set_row_style(index + 2, len(data) - 1)
+            self.__set_row_format(index + 2, format_columns)
+        self.__auto_width()
 
-        def __set_row_format(self, row: int, columns: dict):
-            """
-            设置行样式
+    def save_excel(self, filename: str = None):
+        """
+        保存 excel 文件到本地
+        默认保存到临时目录中
+        """
+        date = datetime.datetime.strftime(datetime.datetime.now(), "%Y%m%d")
+        file_dir = os.path.join(TEMP_DIR, date)
+        if not os.path.exists(file_dir):
+            os.mkdir(file_dir)
+        if not filename:
+            name = hashlib.md5(str(random.random()).encode()).hexdigest() + ".xlsx"
+            filename = os.path.join(file_dir, name)
+        else:
+            name = filename
+            filename = os.path.join(file_dir, filename)
+        self.wb.save(filename)
+        # 返回访问路由
+        return f"{TEMP_URL}/{date}/{name}"
 
-            :param row: 行
-            :param columns: 列数据
-            """
-            for index in columns.get("date_columns", []):
-                self.sheet.cell(row=row, column=index).number_format = "yyyy/mm/dd h:mm:ss"
+    def __set_row_style(self, row: int, max_column: int):
+        """
+        设置行样式
 
-        def __auto_width(self):
-            """
-            设置自适应列宽
-            """
-            # 设置一个字典用于保存列宽数据
-            dims = {}
-            # 遍历表格数据，获取自适应列宽数据
-            for row in self.sheet.rows:
-                for cell in row:
-                    if cell.value:
-                        # 遍历整个表格，把该列所有的单元格文本进行长度对比，找出最长的单元格
-                        # 在对比单元格文本时需要将中文字符识别为1.7个长度，英文字符识别为1个，这里只需要将文本长度直接加上中文字符数量即可
-                        cell_len = 0.7 * len(re.findall('([\u4e00-\u9fa5])', str(cell.value))) + len(str(cell.value))
-                        dims[cell.column] = max((dims.get(cell.column, 0), cell_len))
-            for col, value in dims.items():
-                # 设置列宽，get_column_letter用于获取数字列号对应的字母列号，最后值+2是用来调整最终效果的
-                self.sheet.column_dimensions[get_column_letter(col)].width = value + 10
+        :param row: 行
+        :param max_column: 最大列
+        """
+        for index in range(0, max_column):
+            # 设置单元格对齐方式
+            # Alignment(horizontal=水平对齐模式,vertical=垂直对齐模式,text_rotation=旋转角度,wrap_text=是否自动换行)
+            alignment = Alignment(horizontal='center', vertical='center', text_rotation=0, wrap_text=False)
+            self.sheet.cell(row=row, column=index + 1).alignment = alignment
 
-        def close(self):
-            """
-            关闭文件
-            """
-            self.wb.close()
+    def __set_row_format(self, row: int, columns: dict):
+        """
+        设置行样式
+
+        :param row: 行
+        :param columns: 列数据
+        """
+        for index in columns.get("date_columns", []):
+            self.sheet.cell(row=row, column=index).number_format = "yyyy/mm/dd h:mm:ss"
+
+    def __auto_width(self):
+        """
+        设置自适应列宽
+        """
+        # 设置一个字典用于保存列宽数据
+        dims = {}
+        # 遍历表格数据，获取自适应列宽数据
+        for row in self.sheet.rows:
+            for cell in row:
+                if cell.value:
+                    # 遍历整个表格，把该列所有的单元格文本进行长度对比，找出最长的单元格
+                    # 在对比单元格文本时需要将中文字符识别为1.7个长度，英文字符识别为1个，这里只需要将文本长度直接加上中文字符数量即可
+                    cell_len = 0.7 * len(re.findall('([\u4e00-\u9fa5])', str(cell.value))) + len(str(cell.value))
+                    dims[cell.column] = max((dims.get(cell.column, 0), cell_len))
+        for col, value in dims.items():
+            # 设置列宽，get_column_letter用于获取数字列号对应的字母列号，最后值+2是用来调整最终效果的
+            self.sheet.column_dimensions[get_column_letter(col)].width = value + 10
+
+    def close(self):
+        """
+        关闭文件
+        """
+        self.wb.close()
 
 
 if __name__ == '__main__':
---
+++ utils/excel/import_manage.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2023/4/13 17:36
+# @Author  : 冉勇
+# @Site    : 
+# @File    : import_manage.py
+# @Software: PyCharm
+# @desc    : 数据导入管理
+from typing import List
+from fastapi import UploadFile
+from core.exception import CustomException
+from utils import status
+from .excel_manage import ExcelManage
+from utils.file.file_manage import FileManage
+from .write_xlsx import WriteXlsx
+from ..tools import list_dict_find
+from enum import Enum
+
+
+class FieldType(Enum):
+    list = "list"
+    str = "str"
+
+
+class ImportManage:
+    """
+    数据导入管理
+    只支持导入 .xlsx 后缀类型的文件
+    """
+    file_type = ["application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"]
+
+    def __init__(self, file: UploadFile, headers: List[dict]):
+        """
+
+        :param file:
+        :param headers:
+        """
+        self.__table_data = None
+        self.__table_header = None
+        self.__filename = None
+        self.errors = []
+        self.success = []
+        self.success_number = 0
+        self.error_number = 0
+        self.check_file_type(file)
+        self.file = file
+        self.headers = headers
+
+    @classmethod
+    def check_file_type(cls, file: UploadFile) -> None:
+        """
+        验证文件类型
+        :param file:
+        :return:
+        """
+        if file.content_type not in cls.file_type:
+            raise CustomException(msg="文件类型必须是xlsx类型", code=status.HTTP_ERROR)
+
+    async def get_table_data(self) -> None:
+        """
+        获取表格数据与表头
+        :return:
+        """
+        self.__filename = await FileManage.save_tmp_file(self.file)
+        es = ExcelManage()
+        es.open_sheet(self.__filename, read_only=True)
+        self.__table_header = es.get_header(1, len(self.headers), asterisk=True)
+        self.__table_data = es.readlines(min_row=2, max_col=len(self.headers))
+        es.close()
+
+    def _check_row(self, row: list) -> tuple:
+        """
+        检查行数据
+        检查条件：
+        1、检查是否为必填项
+        2、检查是否为选项列表
+        3、检查是否符合规则
+        :param row: 列表形式的一行数据（每个元素为单元格的值）。
+        :return:
+        代码解释：
+        首先定义一个空字典 data，用于存储转换后的数据。
+        然后循环遍历每个单元格 cell，根据单元格所在列的配置信息 field 进行不同的处理：
+        1、如果单元格的值为空，但是该列必填，则返回 False 和错误提示信息。
+        2、如果单元格有值，且该列是选项列表，则检查该值是否为选项列表中的合法值，如果是，则将其转换为选项列表中对应的值 value；否则返回 False 和错误提示信息。
+        3、如果单元格有值，且该列有规则验证，则逐个调用规则函数，验证该值是否符合规则，如果不符合则返回 False 和错误提示信息。
+        4、如果单元格有值，且该列没有规则验证，则将单元格的值赋给对应的字段 key 并加入 data 中。如果该列是列表类型，则需要将值转换成列表形式。
+        最后将该行原始数据列表 old_data_list 也加入 data 中，并返回 True 和转换后的字典数据。
+        """
+        data = {}
+        for index, cell in enumerate(row):
+            value = cell
+            field = self.headers[index]
+            label = self.__table_header[index]
+            if not cell and field.get("required", False):
+                return False, f"{label}不能为空!"
+            elif field.get("options", []) and cell:
+                item = list_dict_find(field.get("options", []), "label", cell)
+                if item:
+                    value = item.get("value")
+                else:
+                    return False, f"请选择正确的{label}"
+            elif field.get("rules", []) and cell:
+                rules = field.get("rules")
+                for validator in rules:
+                    try:
+                        validator(str(cell))
+                    except ValueError as e:
+                        return False, f"{label}:{e.__str__()}"
+            if value:
+                field_type = field.get("type", FieldType.str)
+                if field_type == FieldType.list:
+                    data[field.get("field")] = [value]
+                elif field_type == FieldType.str:
+                    data[field.get("field")] = str(value)
+            else:
+                data[field.get("field")] = str(value)
+        data["old_data_list"] = row
+        return True, data
+
+    def generate_error_url(self) -> str:
+        """
+        生成用户导入失败数据的 Excel 文件，并返回该文件的链接
+        :return:
+        代码解释：
+        首先判断错误数量 self.error_number 是否小于等于 0，如果是则直接返回空字符串。
+        如果错误数量大于 0，创建一个 WriteXlsx 的对象 em，并调用该对象的 generate_template 方法生成 Excel 文件的头部和表格格式。
+        generate_template 方法会根据传入的 headers，自动生成表头信息，并根据字段类型、选项以及保留小数位等参数设置对应单元格的格式。max_row 参数表示表格最大行数。
+        接着将错误信息 self.errors 写入 Excel 文件中，使用 em 的 write_list 方法将错误列表写入到表格中。
+        最后调用 em 对象的 close 方法关闭 Excel 文件，并返回文件链接 em.file_url。
+        """
+        if self.error_number <= 0:
+            return ""
+        em = WriteXlsx(sheet_name="用户导入失败数据")
+        em.generate_template(self.headers, max_row=self.error_number)
+        em.write_list(self.errors)
+        em.close()
+        return em.file_url
+
+    def add_error_data(self, row: dict) -> None:
+        """
+        将导入数据中有错误的行添加到错误列表并更新错误数量和成功数量
+        :param row: 字典形式的一行数据
+        :return:
+        代码解释：
+        首先将错误数据 row 加入到错误列表 self.errors 中。
+        然后将错误数量 self.error_number 加 1，表示当前导入数据中有一个错误。
+        最后将成功数量 self.success_number 减 1，表示当前导入数据的总成功数量减 1。
+        """
+        self.errors.append(row)
+        self.error_number += 1
+        self.success_number -= 1
