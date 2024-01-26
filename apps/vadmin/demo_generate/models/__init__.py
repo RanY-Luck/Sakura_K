3fa6b7c27b3162e765ad8c94dabc41143f617291
luck_baby <ranyong1209@gmail.com>
2024-01-26 17:27:19 +0800

    🎈 perf(Remove auto-generated code demos): 移除自动生成代码演示

--- apps/vadmin/demo_generate/models/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time     : 2024/1/26 16:20
-# @Author   : 冉勇
-# @File     : __init__.py.py
-# @Software : PyCharm
-# @Desc     :
-
-from .demo_generate import VadminAutomatic
