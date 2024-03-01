24fcf3dc53e50ad7ce4c9fd6b37eb61728d8d5d7
luck_baby <ranyong1209@gmail.com>
2024-03-01 17:43:20 +0800

    ✨ Feat:(Added stress test Demo): 增加压力测试Demo

---
+++ demo.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time     : 2024/3/1 16:26
+# @Author   : 冉勇
+# @File     : demo.py
+# @Software : PyCharm
+# @Desc     :
+import asyncio
+import time
+
+import atomic_bomb_engine
+
+
+async def listen_for_messages():
+    iterator = atomic_bomb_engine.StatusListenIter()
+    for message in iterator:
+        if message:
+            print(message)
+        else:
+            await asyncio.sleep(0.3)
+
+
+async def run_performance_test():
+    print("开始压测")
+    result = await atomic_bomb_engine.run_async(
+        url="https:baidu.com",
+        method="GET",
+        test_duration_secs=300,
+        concurrent_requests=200,
+        timeout_secs=10,
+        verbose=False,
+    )
+    print(time.ctime(), result)
+
+
+async def main():
+    await asyncio.gather(
+        listen_for_messages(),
+        run_performance_test(),
+    )
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
