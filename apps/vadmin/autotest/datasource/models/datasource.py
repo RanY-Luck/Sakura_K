01ba073dbbb9a8f2b42d1ab3dc7e5bba2a06f82b
luck_baby <ranyong1209@gmail.com>
2024-04-25 17:03:25 +0800

    ✨ Feat(New data type table): 新增data_type表

--- apps/vadmin/autotest/datasource/models/datasource.py
+++ apps/vadmin/autotest/datasource/models/datasource.py
@@ -12,12 +12,19 @@ from apps.vadmin.auth.models import VadminUser
 from db.db_base import BaseModel
 
 
+class DataType(BaseModel):
+    __tablename__ = "data_type"
+    __table_args__ = ({'comment': '数据源类型表'})
+
+    type_name: Mapped[str] = mapped_column(String(10), comment="数据源名称")
+    type_id: Mapped[int] = mapped_column(Integer, index=True, comment="类型：1 Mysql", default=1)
+
+
 class DataSourceInfo(BaseModel):
     __tablename__ = "data_source"
     __table_args__ = ({'comment': '数据源表'})
 
     data_name: Mapped[str] = mapped_column(String(10), index=True, comment="数据源名称")
-    type: Mapped[int] = mapped_column(Integer, comment="类型：1 Mysql", default=1)
     host: Mapped[str] = mapped_column(String(255), comment="IP")
     port: Mapped[int] = mapped_column(Integer, comment="端口")
     username: Mapped[str] = mapped_column(String(255), comment="用户名")
@@ -27,4 +34,10 @@ class DataSourceInfo(BaseModel):
         ForeignKey("vadmin_auth_user.id", ondelete='RESTRICT'),
         comment="创建人"
     )
+    type_id: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey("data_type.type_id", ondelete='RESTRICT'),
+        comment="类型"
+    )
     create_user: Mapped[VadminUser] = relationship(foreign_keys=create_user_id)
+    type: Mapped[DataType] = relationship(foreign_keys=type_id)
