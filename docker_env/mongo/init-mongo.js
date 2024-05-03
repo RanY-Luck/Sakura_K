7659689c3ab66815b5c7ca014c4a2e0e554c76a3
ranyong <ranyong1209@gmail.com>
2024-05-03 13:54:52 +0800

    ✨ Feat(Add docker-compose file): 新增 docker-compose文件

--- docker_env/mongo/init-mongo.js
+++ docker_env/mongo/init-mongo.js
@@ -1,7 +1,7 @@
-db = db.getSiblingDB('sakura'); // 切换到要创建的数据库
+db = db.getSiblingDB('admin');
 
 db.createUser({
-    user: 'root',
+    user: 'admin',
     pwd: '123456',
     roles: ["readWrite", "dbAdmin"]
 });
