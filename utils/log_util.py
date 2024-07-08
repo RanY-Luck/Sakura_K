e7e0a4fefe9708a139eda2ad782e9a2507002e68
ranyong <ranyong1209@gmail.com>
2024-07-08 12:25:01 +0800

    🦄 Refactor(Refactor the logging utility class): 重构日志工具类

--- utils/log_util.py
+++ utils/log_util.py
@@ -18,5 +18,24 @@ if not os.path.exists(log_path):
 
 # 仅保存 error 日志
 log_path_error = os.path.join(log_path, f'{time.strftime("%Y-%m-%d")}_error.log')
+log_path_info = os.path.join(log_path, f'{time.strftime("%Y-%m-%d")}_info.log')
 
-logger.add(log_path_error, rotation="50MB", encoding="utf-8", enqueue=True, compression="zip")
+logger.add(
+    log_path_info,
+    rotation="00:00",
+    retention=10,
+    enqueue=True,
+    encoding="UTF-8",
+    compression="zip",
+    level="INFO"
+)
+
+logger.add(
+    log_path_error,
+    rotation="00:00",
+    retention=10,
+    enqueue=True,
+    encoding="UTF-8",
+    compression="zip",
+    level="ERROR"
+)
