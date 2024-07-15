050ddcd5b1daee3f8c035d59962c1bfb1ff5f881
ranyong <ranyong1209@gmail.com>
2024-07-15 10:19:39 +0800

    ✨ Feat(New parameter to manage related routes): 新增参数管理相关路由

--- app.py
+++ app.py
@@ -15,5 +15,6 @@ if __name__ == '__main__':
         host=AppConfig.app_host,
         port=AppConfig.app_port,
         root_path=AppConfig.app_root_path,
-        reload=AppConfig.app_reload
+        reload=AppConfig.app_reload,
+        access_log=False
     )
