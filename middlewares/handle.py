a555734f438a35da055e51b4f5d761d8d85e445d
ranyong <ranyong1209@gmail.com>
2024-08-09 10:46:33 +0800

    ✨ Feat(Add gzip compression middleware): 增加gzip压缩中间件

--- middlewares/handle.py
+++ middlewares/handle.py
@@ -8,6 +8,7 @@
 # @desc    : 全局中间件
 from fastapi import FastAPI
 from middlewares.cors_middleware import add_cors_middleware, register_request_log_middleware
+from middlewares.gzip_middleware import add_gzip_middleware
 
 
 def handle_middleware(app: FastAPI):
@@ -18,4 +19,5 @@ def handle_middleware(app: FastAPI):
     add_cors_middleware(app)
     # 加载记录请求日志中间件
     register_request_log_middleware(app)
-
+    # 加载gzip压缩中间件
+    add_gzip_middleware(app)
