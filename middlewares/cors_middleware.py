5f1ec294a1b003c3afbd556aa4a085fdf773b19a
ranyong <ranyong1209@gmail.com>
2024-08-20 16:45:02 +0800

    🎈 Perf(Optimize - Add lots of comments to make the code more readable): 优化代码-增加大量注释，提高代码可读性

--- middlewares/cors_middleware.py
+++ middlewares/cors_middleware.py
@@ -27,10 +27,15 @@ def add_cors_middleware(app: FastAPI):
 
     # 后台api允许跨域
     app.add_middleware(
+        # 跨域中间件
         CORSMiddleware,
+        # 允许跨域的url
         allow_origins=origins,
+        # 允许跨域请求携带cookie
         allow_credentials=True,
+        # 允许跨域请求的方法
         allow_methods=["*"],
+        # 允许跨域请求的header
         allow_headers=["*"]
     )
 
@@ -39,9 +44,13 @@ def write_request_log(request: Request, response: Response):
     """
     写入log
     """
+    # 请求方法
     http_version = f"http/{request.scope['http_version']}"
+    # 请求内容
     content_length = response.raw_headers[0][1]
+    # 处理时间
     process_time = response.headers["X-Process-Time"]
+    # 日志内容
     content = f"basehttp.log_message:\n '请求方法:{request.method} 请求地址:{request.url} {http_version}' 状态码:{response.status_code} {response.charset} {content_length} 耗时:{process_time}秒"
     logger.info(content)
 
@@ -53,11 +62,18 @@ def register_request_log_middleware(app: FastAPI):
     :return:
     """
 
+    # 请求日志中间件
     @app.middleware("http")
     async def request_log_middleware(request: Request, call_next):
+        # 请求开始时间
         start_time = time.time()
+        # 调用下一个中间件
         response = await call_next(request)
+        # 请求结束时间
         process_time = time.time() - start_time
+        # 写入日志
         response.headers["X-Process-Time"] = str(process_time)
+        # 写入日志
         write_request_log(request, response)
+        # 返回响应
         return response
