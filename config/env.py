e74b2b7c879323469a13039c4f90b136ecc23e2e
ranyong <ranyong1209@gmail.com>
2024-08-26 17:37:14 +0800

    ✨ Feat(Add docker-compose to test successful deployment): 增加 docker-compose测试部署成功

---
+++ config/env.py
@@ -0,0 +1,212 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2024/7/5 10:29
+# @Author  : 冉勇
+# @Site    : 
+# @File    : env.py
+# @Software: PyCharm
+# @desc    : 应用配置
+import os
+import sys
+import argparse
+from pydantic_settings import BaseSettings
+from functools import lru_cache
+from dotenv import load_dotenv
+
+
+class AppSettings(BaseSettings):
+    """
+    应用配置
+    """
+    app_env: str = 'dev'
+    app_name: str = 'Sakura_k'
+    app_root_path: str = '/dev-api'
+    app_host: str = '0.0.0.0'
+    app_port: int = 9099
+    app_version: str = '0.0.1'
+    app_reload: bool = True
+    app_ip_location_query: bool = True
+    app_same_time_login: bool = True
+
+
+class JwtSettings(BaseSettings):
+    """
+    Jwt配置
+    """
+    jwt_secret_key: str = 'b01c66dc2c58dc6a0aabfe2144256be36226de378bf87f72c0c795dda67f4d55'
+    jwt_algorithm: str = 'HS256'
+    jwt_expire_minutes: int = 1440
+    jwt_redis_expire_minutes: int = 30
+
+
+class DataBaseSettings(BaseSettings):
+    """
+    数据库配置
+    """
+    db_host: str = '127.0.0.1'
+    db_port: int = 3306
+    db_username: str = 'root'
+    db_password: str = '123456'
+    db_database: str = 'ruoyi-fastapi'
+    db_echo: bool = True
+    db_max_overflow: int = 10
+    db_pool_size: int = 50
+    db_pool_recycle: int = 3600
+    db_pool_timeout: int = 30
+
+
+class RedisSettings(BaseSettings):
+    """
+    Redis配置
+    """
+    redis_host: str = '127.0.0.1'
+    redis_port: int = 6379
+    redis_username: str = ''
+    redis_password: str = ''
+    redis_database: int = 11
+
+
+class UploadSettings:
+    """
+    上传配置
+    """
+    UPLOAD_PREFIX = '/profile'
+    UPLOAD_PATH = 'vf_admin/upload_path'
+    UPLOAD_MACHINE = 'A'
+    DEFAULT_ALLOWED_EXTENSION = [
+        # 图片
+        "bmp", "gif", "jpg", "jpeg", "png",
+        # word excel powerpoint
+        "doc", "docx", "xls", "xlsx", "ppt", "pptx", "html", "htm", "txt",
+        # 压缩文件
+        "rar", "zip", "gz", "bz2",
+        # 视频格式
+        "mp4", "avi", "rmvb",
+        # pdf
+        "pdf"
+    ]
+    DOWNLOAD_PATH = 'vf_admin/download_path'
+
+    def __init__(self):
+        if not os.path.exists(self.UPLOAD_PATH):
+            os.makedirs(self.UPLOAD_PATH)
+        if not os.path.exists(self.DOWNLOAD_PATH):
+            os.makedirs(self.DOWNLOAD_PATH)
+
+
+class CachePathConfig:
+    """
+    缓存目录配置
+    """
+    PATH = os.path.join(os.path.abspath(os.getcwd()), 'caches')
+    PATHSTR = 'caches'
+
+
+class GetConfig:
+    """
+    获取配置
+    """
+
+    def __init__(self):
+        self.parse_cli_args()
+
+    @lru_cache()
+    def get_app_config(self):
+        """
+        获取应用配置
+        """
+        # 实例化应用配置模型
+        return AppSettings()
+
+    @lru_cache()
+    def get_jwt_config(self):
+        """
+        获取Jwt配置
+        """
+        # 实例化Jwt配置模型
+        return JwtSettings()
+
+    @lru_cache()
+    def get_database_config(self):
+        """
+        获取数据库配置
+        """
+        # 实例化数据库配置模型
+        return DataBaseSettings()
+
+    @lru_cache()
+    def get_redis_config(self):
+        """
+        获取Redis配置
+        """
+        # 实例化Redis配置模型
+        return RedisSettings()
+
+    @lru_cache()
+    def get_upload_config(self):
+        """
+        获取数据库配置
+        """
+        # 实例上传配置
+        return UploadSettings()
+
+    @staticmethod
+    def parse_cli_args():
+        """
+        解析命令行参数
+        """
+        if 'uvicorn' in sys.argv[0]:
+            # 使用uvicorn启动时，命令行参数需要按照uvicorn的文档进行配置，无法自定义参数
+            pass
+        else:
+            # 使用argparse定义命令行参数
+            parser = argparse.ArgumentParser(description='命令行参数')
+            parser.add_argument('--env', type=str, default='', help='运行环境 dev测试环境 prod生产环境 docker容器环境')
+            # 解析命令行参数
+            args = parser.parse_args()
+            # 设置环境变量，如果未设置命令行参数，默认APP_ENV为dev
+            os.environ['APP_ENV'] = args.env if args.env else 'dev'
+        # 读取运行环境
+        run_env = os.environ.get('APP_ENV', '')
+        # 运行环境未指定时默认加载.env.dev
+        env_file = '.env.dev'
+        # 运行环境不为空时按命令行参数加载对应.env文件
+        if run_env != '':
+            env_file = f'.env.{run_env}'
+        # 加载配置
+        load_dotenv(env_file)
+
+
+PROJECT_DESCRIPTION: str = """
+🎉 sakura_k 管理员接口汇总 🎉
+本项目基于Fastapi与Vue3+Typescript+Vite4+element-plus的基础项目 前端基于vue-element-plus-admin框架开发
+#### Description/说明
+<details>
+<summary>点击展开/Click to expand</summary>
+> [中文/Chinese]
+- Sakura_K测试平台，更多功能正在开发中。
+- 本项目开源在[GitHub：sakura_k](https://github.com/ranyong1997/Sakura_K)。
+- 本项目仅供学习交流使用，严禁用于违法用途，如有侵权请联系作者。
+</details>
+#### Contact author/联系作者
+<details>
+<summary>点击展开/Click to expand</summary>
+- WeChat: RanY_Luck
+- Email: [ranyong1209@gmail.com](mailto:ranyong1209@gmail.com)
+- Github: [✶  🎀  GitHub地址  🎀  ✶](https://github.com/ranyong1997)
+- 联系我: ![微信二维码](https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fsafe-img.xhscdn.com%2Fbw1%2F4bb5e771-42f5-47ce-952b-c122c611905a%3FimageView2%2F2%2Fw%2F1080%2Fformat%2Fjpg&refer=http%3A%2F%2Fsafe-img.xhscdn.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1701941750&t=3271891773bfa092923625a10c2cc7d5)
+</details>
+"""
+
+# 实例化获取配置类
+get_config = GetConfig()
+# 应用配置
+AppConfig = get_config.get_app_config()
+# Jwt配置
+JwtConfig = get_config.get_jwt_config()
+# 数据库配置
+DataBaseConfig = get_config.get_database_config()
+# Redis配置
+RedisConfig = get_config.get_redis_config()
+# 上传配置
+UploadConfig = get_config.get_upload_config()
