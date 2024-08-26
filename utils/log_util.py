aa11fd06d1f08d4b3d89dc26f918c8615282065e
ranyong <ranyong1209@gmail.com>
2024-08-26 14:32:13 +0800

    Revert "🎈 Perf(Optimizing log printing): 优化日志打印"
    This reverts commit eb1e731610aa0c6cdfa7676ef64da76568368146.

--- utils/log_util.py
+++ utils/log_util.py
@@ -7,7 +7,6 @@
 # @Software: PyCharm
 # @desc    : 日志工具
 import os
-import sys
 import time
 import datetime
 import zipfile
@@ -19,32 +18,10 @@ log_path = os.path.join(os.getcwd(), 'logs')
 if not os.path.exists(log_path):
     os.mkdir(log_path)
 
-# 日志名称
 log_path_info = os.path.join(log_path, f'{time.strftime("%Y-%m-%d")}_info.log')
 log_path_error = os.path.join(log_path, f'{time.strftime("%Y-%m-%d")}_error.log')
 log_path_warn = os.path.join(log_path, f'{time.strftime("%Y-%m-%d")}_warn.log')
 
-# 移除默认的处理器
-logger.remove()
-
-# 添加控制台输出，保留颜色
-logger.add(
-    sys.stderr,
-    format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
-    level="INFO"
-)
-logger.add(
-    sys.stderr,
-    format="<red>{time:YYYY-MM-DD HH:mm:ss}</red> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
-    level="ERROR"
-)
-logger.add(
-    sys.stderr,
-    format="<yellow>{time:YYYY-MM-DD HH:mm:ss}</yellow> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
-    level="ERROR"
-)
-
-# 保留你原有的文件输出配置
 logger.add(
     log_path_info,
     retention=10,
@@ -52,6 +29,7 @@ logger.add(
     encoding="UTF-8",
     level="INFO"
 )
+
 logger.add(
     log_path_error,
     retention=10,
@@ -59,6 +37,7 @@ logger.add(
     encoding="UTF-8",
     level="ERROR"
 )
+
 logger.add(
     log_path_warn,
     retention=10,
