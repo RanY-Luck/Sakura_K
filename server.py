992c8a423b21a3294fadca1cc434ab983ca814b3
ranyong <ranyong1209@gmail.com>
2024-08-27 18:19:12 +0800

    ✨ Feat(Add static files to directly access the front-end artifacts): 新增静态文件，直接访问前端产物

--- server.py
+++ server.py
@@ -6,8 +6,10 @@
 # @File    : server.py
 # @Software: PyCharm
 # @desc    :
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from contextlib import asynccontextmanager
+from starlette.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
 from sub_applications.handle import handle_sub_applications
 from middlewares.handle import handle_middleware
 from exceptions.handle import handle_exception
@@ -102,3 +104,11 @@ controller_list = [
 # 加载路由
 for controller in controller_list:
     app.include_router(router=controller.get('router'), tags=controller.get('tags'))
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="static/dist")
+
+
+@app.get("/")
+async def serve_spa(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
