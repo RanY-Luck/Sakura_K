da1088f679fca84147e619cf1e5cb5d7692e61d6
ranyong <ranyong1209@gmail.com>
2024-10-23 00:37:51 +0800

    🦄 Refactor(Due to a low-level bug in fastapi query parameter model, the rollback query parameter model declaration method is as_query):因fastapi查询参数模型底层存在bug，回滚查询参数模型声明方式为as_query

--- module_admin/controller/log_controller.py
+++ module_admin/controller/log_controller.py
@@ -6,17 +6,25 @@
 # @File    : log_controller.py
 # @Software: PyCharm
 # @desc    : 日志管理相关接口
-from fastapi import Depends, APIRouter
+from fastapi import APIRouter, Depends, Request
+from sqlalchemy.ext.asyncio import AsyncSession
 from config.enums import BusinessType
 from config.get_db import get_db
 from module_admin.annotation.log_annotation import Log
-from module_admin.service.login_service import LoginService
-from module_admin.service.log_service import *
-from utils.response_util import *
-from utils.log_util import logger
-from utils.page_util import *
-from utils.common_util import bytes2file_response
 from module_admin.aspect.interface_auth import CheckUserInterfaceAuth
+from module_admin.entity.vo.log_vo import (
+    DeleteLoginLogModel,
+    DeleteOperLogModel,
+    LoginLogPageQueryModel,
+    OperLogPageQueryModel,
+    UnlockUser
+)
+from module_admin.service.log_service import LoginLogService, OperationLogService
+from module_admin.service.login_service import LoginService
+from utils.common_util import bytes2file_response
+from utils.log_util import logger
+from utils.page_util import PageResponseModel
+from utils.response_util import ResponseUtil
 
 logController = APIRouter(prefix='/monitor', dependencies=[Depends(LoginService.get_current_user)])
 
