3e057c2cfa5fde0afb602ed99251e9e4106a7420
luck_baby <ranyong1209@gmail.com>
2025-02-19 15:07:10 +0800

    ✨ Feat(Added trace middleware to enhance log link tracing and response headers): 新增trace中间件强化日志链路追踪和响应头

--- middlewares/handle.py
+++ middlewares/handle.py
@@ -9,6 +9,7 @@
 from fastapi import FastAPI
 from middlewares.cors_middleware import add_cors_middleware, register_request_log_middleware
 from middlewares.gzip_middleware import add_gzip_middleware
+from middlewares.trace_middleware import add_trace_middleware
 
 
 def handle_middleware(app: FastAPI):
@@ -21,3 +22,5 @@ def handle_middleware(app: FastAPI):
     register_request_log_middleware(app)
     # 加载gzip压缩中间件
     add_gzip_middleware(app)
+    # 加载trace中间件
+    add_trace_middleware(app)
---
+++ middlewares/trace_middleware/ctx.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time     : 2025/2/19 14:38
+# @Author   : 冉勇
+# @File     : ctx.py
+# @Software : PyCharm
+# @Desc     :
+import contextvars
+from uuid import uuid4
+
+CTX_REQUEST_ID: contextvars.ContextVar[str] = contextvars.ContextVar('request-id', default='')
+
+
+class TraceCtx:
+    @staticmethod
+    def set_id():
+        _id = uuid4().hex
+        CTX_REQUEST_ID.set(_id)
+        return _id
+
+    @staticmethod
+    def get_id():
+        return CTX_REQUEST_ID.get()
